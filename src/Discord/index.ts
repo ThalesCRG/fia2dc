3af2c65a5b4b7eaa2f8d5df13b4e6895d0253f29
Stefan <88635336+ThalesCRG@users.noreply.github.com>
2022-09-11 19:54:11 +0200

    fix for single page documents

--- src/Discord/index.ts
+++ src/Discord/index.ts
@@ -74,27 +74,28 @@ export class DiscordWebhook {
           },
         ],
       };
-      images.forEach((thisimage) => {
-        embedMessage.embeds.push({
-          URL: `${HOME_URI + encodeURI(images[0].name)}`,
-          title: "The FIA just released a new File",
-          description:
-            "The FIA has just released a new Document regarding Formula One. Let me just post it here..",
-          fields: [
-            {
-              name: "File name:",
-              value: file.split(path.sep).pop() ?? "not found",
-              inline: true,
-            },
-            {
-              name: "File size:",
-              value: "" + stats.size,
-              inline: true,
-            },
-          ],
-          timestamp: new Date(stats.birthtime).toISOString(),
-          image: { url: HOME_URI + encodeURI(thisimage.name) },
-        });
+      images.forEach((thisimage, index) => {
+        if (index !== 0)
+          embedMessage.embeds.push({
+            URL: `${HOME_URI + encodeURI(images[0].name)}`,
+            title: "The FIA just released a new File",
+            description:
+              "The FIA has just released a new Document regarding Formula One. Let me just post it here..",
+            fields: [
+              {
+                name: "File name:",
+                value: file.split(path.sep).pop() ?? "not found",
+                inline: true,
+              },
+              {
+                name: "File size:",
+                value: "" + stats.size,
+                inline: true,
+              },
+            ],
+            timestamp: new Date(stats.birthtime).toISOString(),
+            image: { url: HOME_URI + encodeURI(thisimage.name) },
+          });
       });
 
       await this.webhookClient.send(embedMessage);
